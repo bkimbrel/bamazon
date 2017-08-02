c5963e2465aa358ded07c6c84694a7a859ee0808
bkimbrel <bkimbrel2@gmail.com>
2017-08-01 19:58:29 -0600

    inital file structure

---
+++ bamazon.sql
@@ -0,0 +1,9 @@
+create database if not exists bamazon;
+
+create table if not exists products(
+item_id int not null auto_increment,
+product_name  varchar(50) not null, 
+department_name varchar(50) not null,
+price int not null,
+quantity int not null
+);
